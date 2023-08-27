9882061229817c8b64e46c66a073706a905feb17
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-08-27 11:17:45 -0500

    Adjusted camera bounds

--- Assets/Scripts/CreatureTypes/Chicken.cs
+++ Assets/Scripts/CreatureTypes/Chicken.cs
@@ -22,6 +22,8 @@ public class Chicken : Creature, IAttack
                     {
                         targetCreature = null;
                     }
+
+                    return;
                 }
             }
         }
