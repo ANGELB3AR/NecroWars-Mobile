93b92058027454726ca2bcb4604af7467361dd68
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-02 14:38:13 -0500

    Created initial game manager script

--- Assets/Scripts/Core/GameManager.cs
+++ Assets/Scripts/Core/GameManager.cs
@@ -1,8 +1,45 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GameManager : Singleton<GameManager>
 {
+    public GameState State;
 
+    public static event Action<GameState> OnGameStateChanged;
+
+    private void Start()
+    {
+        UpdateGameState(GameState.MainMenu);
+    }
+
+    public void UpdateGameState(GameState newState)
+    {
+        State = newState;
+
+        switch (newState)
+        {
+            case GameState.MainMenu:
+                break;
+            case GameState.OptionsMenu:
+                break;
+            case GameState.Playing:
+                break;
+            case GameState.Paused:
+                break;
+            default:
+                break;
+        }
+
+        OnGameStateChanged?.Invoke(newState);
+    }
+}
+
+public enum GameState
+{
+    MainMenu,
+    OptionsMenu,
+    Playing,
+    Paused
 }
