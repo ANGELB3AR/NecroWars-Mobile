53def25df1db943c672ab745ef0b43931a609ce9
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-16 17:09:02 -0500

    Started custom editor script

---
+++ Assets/Scripts/Editor/CreatureCreatorMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector.Editor;
+
+public class CreatureCreatorMenu : OdinMenuEditorWindow
+{
+    protected override OdinMenuTree BuildMenuTree()
+    {
+        throw new System.NotImplementedException();
+    }
+}
