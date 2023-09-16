207e4fb98714661ee0505ea6990b5a11e96da80f
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-16 17:55:12 -0500

    Started adding to creature creator menu

--- Assets/Scripts/Editor/CreatureCreatorMenu.cs
+++ Assets/Scripts/Editor/CreatureCreatorMenu.cs
@@ -1,12 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
 using Sirenix.OdinInspector.Editor;
+using Sirenix.Utilities.Editor;
+using Sirenix.Utilities;
 
 public class CreatureCreatorMenu : OdinMenuEditorWindow
 {
+    [MenuItem("Tools/Glyphix/CreatureCreator")]
+    private static void Open()
+    {
+        var window = GetWindow<CreatureCreatorMenu>();
+        window.position = GUIHelper.GetEditorWindowRect().AlignCenter(800, 500);
+    }
+
     protected override OdinMenuTree BuildMenuTree()
     {
-        throw new System.NotImplementedException();
+        var tree = new OdinMenuTree();
+
+        tree.AddAllAssetsAtPath("Creatures", "Assets/Prefabs/Creatures/0 - Prefabs", typeof(Creature));
+
+        return tree;
     }
 }
