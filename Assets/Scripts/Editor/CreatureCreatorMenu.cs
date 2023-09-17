21b7913b22a1d5ad1349ebe42ef2122745db7410
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-16 19:06:15 -0500

    Programmed initial creature creator menu to use scriptable objects

--- Assets/Scripts/Editor/CreatureCreatorMenu.cs
+++ Assets/Scripts/Editor/CreatureCreatorMenu.cs
@@ -1,11 +1,17 @@
 using UnityEngine;
 using UnityEditor;
-using Sirenix.OdinInspector.Editor;
-using Sirenix.Utilities.Editor;
 using Sirenix.Utilities;
+using Sirenix.Utilities.Editor;
+using Sirenix.OdinInspector;
+using Sirenix.OdinInspector.Editor;
 
 public class CreatureCreatorMenu : OdinMenuEditorWindow
 {
+    private CreateNewCreature createNewCreature;
+
+    public static readonly string folderPath = "Assets/Prefabs/Creatures/0 - Creature Data";
+
+
     [MenuItem("Tools/Glyphix/CreatureCreator")]
     private static void Open()
     {
@@ -17,8 +23,65 @@ public class CreatureCreatorMenu : OdinMenuEditorWindow
     {
         var tree = new OdinMenuTree();
 
-        tree.AddAllAssetsAtPath("Creatures", "Assets/Prefabs/Creatures/0 - Prefabs", typeof(Creature));
+        createNewCreature = new CreateNewCreature();
+        tree.Add("Create New", createNewCreature);
+        tree.AddAllAssetsAtPath("Creatures", folderPath, typeof(CreatureData));
 
         return tree;
     }
+
+    protected override void OnDestroy()
+    {
+        base.OnDestroy();
+
+        if (createNewCreature != null)
+        {
+            DestroyImmediate(createNewCreature.creatureData);
+        }
+    }
+
+    protected override void OnBeginDrawEditors()
+    {
+        // Gets currently selected item
+        OdinMenuTreeSelection selected = this.MenuTree.Selection;
+
+        SirenixEditorGUI.BeginHorizontalToolbar();
+        {
+            GUILayout.FlexibleSpace();
+
+            if (SirenixEditorGUI.ToolbarButton("Delete Current"))
+            {
+                CreatureData asset = selected.SelectedValue as CreatureData;
+                string path = AssetDatabase.GetAssetPath(asset);
+                AssetDatabase.DeleteAsset(path);
+                AssetDatabase.SaveAssets();
+            }
+        }
+        SirenixEditorGUI.EndHorizontalToolbar();
+    }
+
+
+    public class CreateNewCreature
+    {
+        [InlineEditor(ObjectFieldMode = InlineEditorObjectFieldModes.Hidden)]
+        public CreatureData creatureData;
+    
+        public CreateNewCreature()
+        {
+            creatureData = ScriptableObject.CreateInstance<CreatureData>();
+            creatureData.name = "New Creature";
+        }
+
+        [Button("Create New Creature")]
+        private void CreateNewCreatureData()
+        {
+            AssetDatabase.CreateAsset(creatureData, CreatureCreatorMenu.folderPath + creatureData.name + ".asset");
+            AssetDatabase.SaveAssets();
+
+            // Creates new instance
+            creatureData = ScriptableObject.CreateInstance<CreatureData>();
+            creatureData.name = "New Creature";
+        }
+    }
 }
+
