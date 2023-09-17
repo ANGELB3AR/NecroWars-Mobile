ba9af33118862e0d96f34db85d8530dbee7e70ef
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-16 20:27:49 -0500

    Started formatting creature creator menu

--- Assets/Scripts/CreatureData.cs
+++ Assets/Scripts/CreatureData.cs
@@ -8,13 +8,15 @@ public class CreatureData : ScriptableObject
     public string creatureName;
     public GameObject creatureModel;
     public Material resurrectedMaterial;
-    public float attackCooldown;
-    [PropertyRange(0.1f, 5f)]
-    public float attackRange = 0.1f;
-
-    public float attackDamage;
-    public bool hasBonusAttack;
-    [ShowIf(nameof(hasBonusAttack))]
-    public float bonusAttackChargeTime;
     public AnimationCurve spawnProbabilityCurve;
+    [ProgressBar(0.1f, 300f)] [PropertyTooltip("How many seconds between regular attacks")]
+    public float attackCooldown = 0.5f;
+    [ProgressBar(0.1f, 4.9f)] [PropertyTooltip("How many meters away regular attacks can hit. Limitations are in place to prevent errors.")]
+    public float attackRange = 2f;
+    [ProgressBar(0, 100)]
+    [PropertyTooltip("How much damage a regular attack does")]
+    public float attackDamage = 5f;
+    public bool hasBonusAttack = false;
+    [ShowIf(nameof(hasBonusAttack))] [ProgressBar(0, 300)] [PropertyTooltip("How many seconds between bonus attacks")]
+    public float bonusAttackChargeTime;
 }
