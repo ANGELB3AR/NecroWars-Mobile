87a91bbc6da9b275fc220994ad951f42574898f7
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-19 06:54:15 -0500

    Started setting up configurable bonus attack system

---
+++ Assets/Scripts/BonusAttackSO.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "NewBonusAttack", menuName = "Create New Bonus Attack")]
+public class BonusAttackSO : SerializedScriptableObject
+{
+    public AimType aimType;
+}
+
+public enum AimType
+{
+    Line_FirstHit,
+    Line_HitAll,
+    Area
+}
