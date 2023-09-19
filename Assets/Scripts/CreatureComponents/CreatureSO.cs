7d434209939f0d53d72ce9449c62dc99a349012f
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-09-19 06:13:05 -0500

    Started reformatting CreatureSO

--- Assets/Scripts/CreatureComponents/CreatureSO.cs
+++ Assets/Scripts/CreatureComponents/CreatureSO.cs
@@ -6,10 +6,18 @@ using Sirenix.OdinInspector;
 [CreateAssetMenu(fileName = "New Creature", menuName = "Create New Creature")]
 public class CreatureSO : SerializedScriptableObject
 {
+    [BoxGroup("Main")]
     public string creatureName;
+    [BoxGroup("Main")]
+    [PreviewField]
     public GameObject creatureModel;
-    public AnimationCurve spawnWeight;
+    [BoxGroup("Main")]
+    [PreviewField]
     public Material resurrectedMaterial;
+    [BoxGroup("Main")]
+    public AnimationCurve spawnWeight;
+
+    [TabGroup("Combat", "Normal Attack")]
     public float attackCooldown;
     public float attackRange;
     public float attackDamage;
