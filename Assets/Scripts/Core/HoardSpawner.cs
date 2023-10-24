edb1571f418a88e57913815ecf81827eff893ab0
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-10-24 08:43:36 -0500

    Started working on spawning functions

--- Assets/Scripts/Core/HoardSpawner.cs
+++ Assets/Scripts/Core/HoardSpawner.cs
@@ -8,5 +8,17 @@ public class HoardSpawner : SerializedMonoBehaviour
     [DictionaryDrawerSettings(DisplayMode = DictionaryDisplayOptions.OneLine, KeyLabel = "Creature Type", ValueLabel = "Spawn Curve")]
     [SerializeField] Dictionary<CreatureSO, AnimationCurve> creatureSpawnCurves = new Dictionary<CreatureSO, AnimationCurve>();
 
+    public void CreateRandomizedHoards(int currentLevel)
+    {
+        float totalWeight = 0f;
 
+        foreach (var spawnCurve in creatureSpawnCurves)
+        {
+            AnimationCurve curve = spawnCurve.Value;
+            float weight = curve.Evaluate(currentLevel);
+            totalWeight += weight;
+        }
+
+
+    }
 }
