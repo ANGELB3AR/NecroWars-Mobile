20d076021e1c83da5bffc65a1b6c052d57cf3dc1
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-11-10 10:09:01 -0600

    Tried to add game win delay but coroutine not firing

--- Assets/Scripts/UI/Menus/MenuManager.cs
+++ Assets/Scripts/UI/Menus/MenuManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class MenuManager : Singleton<MenuManager>
@@ -21,10 +22,21 @@ public class MenuManager : Singleton<MenuManager>
 
     private void GameManager_OnGameStateChanged(GameState gameState)
     {
+        if (gameState == GameState.GameWon)
+        {
+            AddDelay(3);
+        }
+
         mainMenu.SetActive(gameState == GameState.MainMenu);
         optionsMenu.SetActive(gameState == GameState.OptionsMenu);
         pauseMenu.SetActive(gameState == GameState.Paused);
         gameWinMenu.SetActive(gameState == GameState.GameWon);
         gameLoseMenu.SetActive(gameState == GameState.GameLost);
+    }
+
+    private IEnumerator AddDelay(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        Debug.Log("Waited some time");
     }
 }
