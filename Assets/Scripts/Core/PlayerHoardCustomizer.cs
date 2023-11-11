f581c5082bb153380a3e4d5b8c660814c129d175
Angel Bowers <ANGELLBOWERS@GMAIL.COM>
2023-11-11 10:58:47 -0600

    Added player hoard customizer class

---
+++ Assets/Scripts/Core/PlayerHoardCustomizer.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHoardCustomizer : MonoBehaviour
+{
+    [SerializeField] private List<CreatureSO> playerStartingHoard = new List<CreatureSO>();
+
+    public void AddNewCreature()
+    {
+
+    }
+
+    public void UpgradeCreature()
+    {
+
+    }
+}
